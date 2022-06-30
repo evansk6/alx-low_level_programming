5473bea6097334cc04dc20e77475e5d13eeef7c7
evansk6 <evanskarega@gmail.com>
2022-06-30 02:43:31 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  * Return: 0 after printing the function
  */
 int main(void)
-{puts("\"programmingis like building a multilingual puzzle");
+{puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
