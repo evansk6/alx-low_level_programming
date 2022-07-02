e77c4625b24943dfd9539263ae9cfb9122dda606
evansk6 <evanskarega@gmail.com>
2022-07-01 23:54:07 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -> assign a random number to the variable n each time it is executed
+ * and print the last digit of the number stored in the variable n
+ * return : always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n)
+	}
+
+	return (0);
+}
