714269798d09ce0e6d233114bf5c999153790510
evansk6 <evanskarega@gmail.com>
2022-07-04 00:15:25 -0700

    print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return : always 0
+ */
+int main(void)
+{
+int n = 0;
+for (; 0 < 10 ; n++)
+putchar('0' + 0);
+putchar("\n");
+return (0);
+}
