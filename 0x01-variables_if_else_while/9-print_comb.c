a2ebd7a6d4be713510a9c745484f5f88351e9e59
evansk6 <evanskarega@gmail.com>
2022-07-04 03:25:10 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,33 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#define N 10.
-
-void print(int*num,int i)
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
 {
-	int n;
-	for (n = 0; n < i; n++);
-	putchar("%d", num(n));
-
-	    putchar("\n");
+int i;
+for (i = 10; i < 20; i++)
+{
+putchar((i % 10) + '0');
+if (i != 19)
+{
+putchar(',');
+putchar(' ');
 }
-int main()
-{
-	int num(N);
-	int *ptr;
-	int temp;
-	int i, n, j;
-	putchar("\n4");
-	   scanf("%d", &n);
-	putchar("\n1234");
-	for (n = 0; n < i; n++)
-		scanf("%d", &num(n));
-	for (j = 1; j <= i; j++) {
-		for (n = 0; n < i-1; n++) {
-			temp = num(n);
-			num(n) = num(n+1);
-			num(n+1) = temp;
-			print(num, i);
-		}
-	}
-	return 0;
+}
+putchar('\n');
+return (0);
 }
