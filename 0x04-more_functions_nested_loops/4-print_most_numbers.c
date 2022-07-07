1737693abd4546835b12f800a8e94870d2ecb000
evansk6 <evanskarega@gmail.com>
2022-07-07 06:12:43 -0700

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -> print numbers
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+	}
+	_putchar('\n');
+}
