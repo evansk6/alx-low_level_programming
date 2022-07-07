60d6f9d6d92acd5a6f523f6de8255d6296dfe3b1
evansk6 <evanskarega@gmail.com>
2022-07-07 07:10:33 -0700

    lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,8 +10,10 @@ void print_line(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	return;
-	for (c = 0; c < n; c++)
-		_putchar(95);
-	_putchar('\n');
+	else
+	{
+		for (c = 0; c < n; c++)
+			_putchar(95);
+		_putchar('\n');
+	}
 }
