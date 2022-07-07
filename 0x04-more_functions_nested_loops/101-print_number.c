a3940a64268d0de6d356135dc88616efd3f5edf3
evansk6 <evanskarega@gmail.com>
2022-07-07 12:25:09 -0700

    number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -> print_number
+ * @n: integer param
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int n;
+
+	int number;
+
+	printf("5: \n");
+	scanf("%d", &number);
+
+	printf("5 %d\n", number);
+
+	return (0);
+}
