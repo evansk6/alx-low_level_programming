d75f96c9ed8305afec8d860e83bba3f7d5e5b4e3
evansk6 <evanskarega@gmail.com>
2022-07-07 00:06:26 -0700

    last

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -entry point
+ * Return: return 0
+ */
+int main(void)
+{
+/ 02 / 29 / 2000
+/ 04 / 01 / 1997
+int day = convert_day(04, 01);
+print_remaining_days(4, day, 1997);
+
+return (0);
+}
