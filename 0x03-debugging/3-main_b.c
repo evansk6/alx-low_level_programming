bf412182e4b0a57f135b74798025e225f5cd8d89
evansk6 <evanskarega@gmail.com>
2022-07-08 07:22:16 -0700

    numb

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - entry point
+ * main -> printing remaining day
+ * @n: day
  * Return: return 0
  */
 int main(void)
 {
-/ 02 / 29 / 2000
-/ 04 / 01 / 1997
+ / 02 / 29 / 2000
+ / 04 / 01 / 1997
 int day = convert_day(04, 01);
 print_remaining_days(4, day, 1997);
 
