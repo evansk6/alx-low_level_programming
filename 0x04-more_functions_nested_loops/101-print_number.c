82191890f12a2c8092f8ba7f7244c16744ffa03b
evansk6 <evanskarega@gmail.com>
2022-07-08 01:39:12 -0700

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_numbers - print int with putchar
  * @n: integer param
+ *
  * Return: void
  */
 
 void print_number(int n)
 {
 unsigned int m;
+
 if (n < 0)
 {
 _putchar('-');
