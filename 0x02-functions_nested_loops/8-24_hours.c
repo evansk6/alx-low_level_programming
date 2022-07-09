635b01c1f082923b1084bef1fb010597a2fb0283
evansk6 <evanskarega@gmail.com>
2022-07-09 01:05:58 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * jack_bauer -> prints 24 hours
  */
@@ -13,7 +13,7 @@ void jack_bauer(void)
 			if (i < 10)
 			{
 				_putchar('0');
-				_putchar(i + '0')
+				_putchar(i + '0');
 			}
 			else if (i >= 10)
 			{
