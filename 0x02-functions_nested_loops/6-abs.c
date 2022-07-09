bbff353e319a0f826a2257eb1b6d33a552cc615c
evansk6 <evanskarega@gmail.com>
2022-07-09 00:31:01 -0700

    lucfh

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _abs -> return absolute value of a number
 * @n: parameter
 * return: absolute value of a number
 */
-int _abs(int)
+int _abs(int n)
 {
 	if (n < 0)
 		n = -n;
