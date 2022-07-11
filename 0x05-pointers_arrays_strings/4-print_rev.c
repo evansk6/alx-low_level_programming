568ed57c46691dcb3b1ac25ebb8d336d6aaab203
evansk6 <evanskarega@gmail.com>
2022-07-11 01:41:18 -0700

    perint

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,12 @@ void print_rev(char *s)
 	int i, n;
 
 	n = 0;
-	while (s([n] != '\0')
+	while (s[n] != '\0')
 		n++;
 
-	For (i = n - 1; i >= 0; i++)
+	for (i = n - 1; i >= 0; i--)
 	{
-		_putchar(s([i]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
