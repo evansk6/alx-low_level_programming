9030514c1554e5d9137ae61ea1a5039c96beb50a
evansk6 <evanskarega@gmail.com>
2022-07-11 23:05:47 -0700

    array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array -> element of an array
+ * @a: param 1
+ * @n: parameter 2
+ * Return: always 0
+ */
+void print_array(int *a, int n)
+{
+	int arr n;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d :", i);
+		scanf("%d ", arr[i];
+	}
+	for (i = 0; i < n; i++)
+	{
+	printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
