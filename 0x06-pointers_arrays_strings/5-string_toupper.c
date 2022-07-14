a13742596926d09471ec644ac81529df310c36e9
evansk6 <evanskarega@gmail.com>
2022-07-13 22:55:57 -0700

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,17 +2,17 @@
 
 /**
  * string_toupper -> convert to uppercase
- * @x: param
+ * @x: string param
  * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
 	int i = 0;
 
 	while (x[i])
 	{
 		if (x[i] >= 97 && x[i] <= 122)
-			x[i] = x[i] -32;
+			x[i] = x[i] - 32;
 		i++;
 	}
 	return (x);
