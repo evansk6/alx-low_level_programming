0b690210bcc0aaf824bd6e94532e171cdd57aad1
evansk6 <evanskarega@gmail.com>
2022-07-18 08:01:12 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr -> function
+ * @s: param
+ * @c: param
+ * Return: string
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
+
+	while (s[a])
+		a++;
+	for (b = 0; b < a; b++)
+	{
+		if (c == s[b])
+			s += b;
+		return (s);
+	}
+	return ('\n');
+}
