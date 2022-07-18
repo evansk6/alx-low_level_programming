03206a1b3eb799b14fbbd708aad73053143b8e20
evansk6 <evanskarega@gmail.com>
2022-07-18 09:26:31 -0700

    lucky

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strspm -> set of bytes
+ * @s: param
+ * @accept: param
+ * Return: string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0, b, t = 0;
+
+	while (accept[a])
+	{
+		b = 0;
+		while (s[b] != 32)
+		{
+			if (accept[a] == s[b])
+			{
+				t++;
+			}
+			b++;
+		}
+		a++;
+	}
+	return (t);
+}
