4d90d2874d1f2fe37949b92a403ff490baf32368
evansk6 <evanskarega@gmail.com>
2022-07-20 11:35:58 -0700

    fact

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,11 +8,8 @@
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else
-	{
-		return (n * factorial (n - 1));
-	}
+	if (n == 0 || n == 1)
+		return (1);
+	return (n * factorial(n - 1));
 }
