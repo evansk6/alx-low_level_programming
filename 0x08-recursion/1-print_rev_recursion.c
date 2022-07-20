9cde95a64a1d272dad37270b79650292541c1d50
evansk6 <evanskarega@gmail.com>
2022-07-20 09:25:43 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -> string in reverse
+ * @s: strig param
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_rev_recursion(s - 1);
+}
