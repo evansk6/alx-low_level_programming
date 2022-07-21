7f982c77e77f30b30b2dc04670ed188469bdc2a4
evansk6 <evanskarega@gmail.com>
2022-07-20 23:05:40 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
+int _sqrt(int, int);
+
 /**
- * helperFunction -> 0
  * _sqrt_recursion -> squre root
- * @num: param
- * @i: param
  * @n: param
  * Return: 0
  */
-int helperFunction(int num, int i)
 int _sqrt_recursion(int n)
 {
-	return (_sqt(n, 1));
+	return (_sqrt(n, 1));
 }
 
 /**
