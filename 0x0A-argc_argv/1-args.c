3696fe65c356e92245fa03ff6698e73b423f75d4
evansk6 <evanskarega@gmail.com>
2022-07-22 09:28:20 -0700

    name arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -> number of argument
+ * @argc: parameter
+ * @argv: param
+ * Return: 0
+ */
+int main(int argc, char *argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
