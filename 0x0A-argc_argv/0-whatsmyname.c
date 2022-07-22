34d2d1ac5c22d8a94686dfeda451b3e17ed5db05
evansk6 <evanskarega@gmail.com>
2022-07-22 09:20:41 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: param
  * Return: 0
  */
-int main(int argc, attribute ((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
