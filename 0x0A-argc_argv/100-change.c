ba1e6400be7dbaffdbaa427997bd070533e08cbe
evansk6 <evanskarega@gmail.com>
2022-07-22 23:40:47 -0700

    changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main -> minimum number of coin
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
 {
 	int result = 0, num, i, j, k;
 
-	for(i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		for (j = 0;argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > '9' || argv[i][j] < '0')
 			{
