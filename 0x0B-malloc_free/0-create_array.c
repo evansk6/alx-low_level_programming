babd778c7e8df0213dcf11498759c7a661979bf7
evansk6 <evanskarega@gmail.com>
2022-07-25 07:16:30 -0700

    strdup

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * create_array -> initialize with special char
+ * @size: array param
+ * @c: array param
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+/**
+ * create_array -> 
