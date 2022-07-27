8479e04200ea1383572028a35d38c77815a823d1
evansk6 <evanskarega@gmail.com>
2022-07-27 00:24:28 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * argstostr -> concanates all argument
+ * @ac: param
+ * @av: param
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int ac, i;
+	char *av;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+	while (av[ac])
+		ac++;
+
+	av = (char *)malloc(i * sizeof(char) + 1);
+	av[i] = '\0';
+	return (av'\n');
+}
